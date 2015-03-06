6db9cdcbdbca70090d4396277fe437d8fe2a4622
Konstantinos Chartomatzis <constchart@gmail.com>
2015-03-06 12:17:19 +0000

    Initial Commit

---
+++ mpiInit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mpiInit
+//
+//  Created by Konstantinos Chartomatzis on 06/03/2015.
+//  Copyright (c) 2015 Konstantinos Chartomatzis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
